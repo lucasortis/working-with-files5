1da956e91104b88509fab260def7cfe70199b6d6
Lucas Ortis <lucasortis01@gmail.com>
2021-09-22 19:01:02 -0300

    Using Directory class to create directories and files

--- WorkingWithFiles5/Program.cs
+++ WorkingWithFiles5/Program.cs
@@ -6,7 +6,28 @@ namespace WorkingWithFiles5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"C:\Users\Lucas Ortis\Downloads";
+
+            try
+            {
+                var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                foreach (string folder in folders)
+                {
+                    Console.WriteLine(folder);
+                }
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                foreach (string file in files)
+                {
+                    Console.WriteLine(file);
+                }
+
+                Directory.CreateDirectory(path + @"/newfolder");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred: " + e.Message);
+            }
         }
     }
 }
